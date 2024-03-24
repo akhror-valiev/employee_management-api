fd6f20f88d7203e968cbceb1e1e800e1ef5282fd
akhror-valiev <ahror.valiev@gmail.com>
2024-03-24 16:31:32 +0500

    Create employee model

---
+++ app/models/employee.rb
@@ -0,0 +1,8 @@
+class Employee < ApplicationRecord
+    validates :first_name, presence: true, length: { maximum: 50 }
+    validates :last_name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :contact_number, presence: true, length: { maximum: 20 }
+    validates :address, presence: true, length: { maximum: 255 }
+end
